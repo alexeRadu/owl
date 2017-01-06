039e55281e9e4172383aa3039bba846da0b1feea
Radu Alexe <alexeradu2007@gmail.com>
2017-01-06 20:25:37 +0000

    simple char read/echoing in ncurses
    Some additional functions have been called when initializing
    ncurses.
    
    For more details see ncurses tutorial chapter 4.
    
    Signed-off-by: Radu Alexe <alexeradu2007@gmail.com>

--- owl.c
+++ owl.c
@@ -2,11 +2,37 @@
 
 int main(int argc, char **argv)
 {
-	initscr();			/* start curses mode */
-	printw("Hello World !!!");	/* print to screen */
-	refresh();			/* print it on the real screen */
-	getch();			/* wait for the user input */
-	endwin();			/* end curses mode */
+	int ch;
+
+	/* curses mode initialization */
+	initscr();	/* start curses mode */
+	raw();		/* disable line buffering */
+	noecho();	/* disable echoing - it should be done manually */
+
+	/* enable reading of functional keys (F1, F2, arrow keys, etc */
+	keypad (stdscr, TRUE);
+
+	printw("Type any character to see it in bold\n");
+	while (1) {
+		ch = getch();
+
+		printw("The pressed key is: ");
+		attron(A_BOLD);
+		printw("%c\n", ch);
+		attroff(A_BOLD);
+		refresh();
+
+		if (ch == KEY_F(1)) {
+			printw("Exiting the editor");
+			refresh();
+			break;
+		}
+	}
+
+	getch();
+
+	/* end curses mode */
+	endwin();
 
 	return 0;
 }
